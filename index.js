d7ed1f91eabfa88197ad7ddc32a7b8380d993b83
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-26 16:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  finalArray = [];
+  for (var n=0; n<4; n++) {
+    finalArray[n] = `${musicians[n]} plays ${instruments[n]}`
+    }
+    return finalArray
+  }
+
+  
+  
+
