b7b95e73d2bb03f8176dc4f246c02add4dd9e474
Tess Guenthner <tessmadeleine@hotmail.com>
2019-02-26 17:04:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  beatlesLoops = [];
+  array = [];
   for (var n=0; n<4; n++) {
-    beatlesLoops[n] = `${musicians[n]} plays ${instruments[n]}`
+    array[n] = `${musicians[n]} plays ${instruments[n]}`
     }
-    return beatlesLoops
+    return array
   }
